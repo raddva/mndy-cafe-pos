d3de1ffe8012a7219e6e788fc0401fedd24eef1c
raddva <111165508+raddva@users.noreply.github.com>
2025-07-25 13:45:52 +0700

    add: create user

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { clsx, type ClassValue } from "clsx";
+import { ChangeEvent } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getImageData(event: ChangeEvent<HTMLInputElement>) {
+  // const dataTransfer = new DataTransfer();
+
+  // Array.from(event.target.files!).forEach((image) =>
+  //   dataTransfer.items.add(image)
+  // );
+
+  // const file = dataTransfer.files[0];
+  // const displayUrl = URL.createObjectURL(event.target.files![0]);
+  const file = event.target.files![0];
+  const displayUrl = URL.createObjectURL(file);
+  return { file, displayUrl };
 }
