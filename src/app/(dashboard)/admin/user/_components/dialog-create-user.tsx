afbea8fcff3f2bf88a4a702398d6ab8f25348d52
raddva <111165508+raddva@users.noreply.github.com>
2025-07-25 15:16:26 +0700

    add: delete user & fix: add user

--- src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -1,103 +1,65 @@
 import {
     INITIAL_CREATE_USER_FORM,
     INITIAL_STATE_CREATE_USER,
-    INITIAL_STATE_UPDATE_USER,
 } from '@/constants/auth-constant';
 import {
     CreateUserForm,
     createUserSchema,
-    UpdateUserForm,
-    updateUserSchema,
 } from '@/validations/auth-validation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { startTransition, useActionState, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { createUser, updateUser } from '../actions';
+import { createUser } from '../actions';
 import { toast } from 'sonner';
 import { Preview } from '@/types/general';
 import FormUser from './form-user';
-import { Profile } from '@/types/auth';
-import { Dialog } from '@radix-ui/react-dialog';
 
-export default function DialogUpdateUser({
-    refetch,
-    currentData,
-    open,
-    handleChangeAction,
-}: {
-    refetch: () => void;
-    currentData?: Profile;
-    open?: boolean;
-    handleChangeAction?: (open: boolean) => void;
-}) {
-    const form = useForm<UpdateUserForm>({
-        resolver: zodResolver(updateUserSchema),
+export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
+    const form = useForm<CreateUserForm>({
+        resolver: zodResolver(createUserSchema),
+        defaultValues: INITIAL_CREATE_USER_FORM,
     });
 
-    const [updateUserState, updateUserAction, isPendingUpdateUser] =
-        useActionState(updateUser, INITIAL_STATE_UPDATE_USER);
+    const [createUserState, createUserAction, isPendingCreateUser] =
+        useActionState(createUser, INITIAL_STATE_CREATE_USER);
 
     const [preview, setPreview] = useState<Preview | undefined>(undefined);
 
     const onSubmit = form.handleSubmit((data) => {
         const formData = new FormData();
-        if (currentData?.avatar_url !== data.avatar_url) {
-            Object.entries(data).forEach(([key, value]) => {
-                formData.append(
-                    key,
-                    key === 'avatar_url' ? preview!.file ?? '' : value,
-                );
-            });
-            formData.append('old_avatar_url', currentData?.avatar_url ?? '');
-        } else {
-            Object.entries(data).forEach(([Key, value]) => {
-                formData.append(Key, value);
-            });
-        }
-        formData.append('id', currentData?.id ?? '');
+        Object.entries(data).forEach(([key, value]) => {
+            formData.append(key, key === 'avatar_url' ? preview!.file ?? '' : value);
+        });
 
         startTransition(() => {
-            updateUserAction(formData);
+            createUserAction(formData);
         });
     });
 
     useEffect(() => {
-        if (updateUserState?.status === 'error') {
-            toast.error('Update User Failed', {
-                description: updateUserState.errors?._form?.[0],
+        if (createUserState?.status === 'error') {
+            toast.error('Create User Failed', {
+                description: createUserState.errors?._form?.[0],
             });
         }
 
-        if (updateUserState?.status === 'success') {
-            toast.success('Update User Success');
+        if (createUserState?.status === 'success') {
+            toast.success('Create User Success');
             form.reset();
-            handleChangeAction?.(false);
+            setPreview(undefined);
+            document.querySelector<HTMLButtonElement>('[data-state="open"]')?.click();
             refetch();
         }
-    }, [updateUserState]);
-
-    useEffect(() => {
-        if (currentData) {
-            form.setValue('name', currentData.name as string);
-            form.setValue('role', currentData.role as string);
-            form.setValue('avatar_url', currentData.avatar_url as string);
-            setPreview({
-                file: new File([], currentData.avatar_url as string),
-                displayUrl: currentData.avatar_url as string,
-            });
-        }
-    }, [currentData]);
+    }, [createUserState]);
 
     return (
-        <Dialog open={open} onOpenChange={handleChangeAction}>
-            <FormUser
-                form={form}
-                onSubmit={onSubmit}
-                isLoading={isPendingUpdateUser}
-                type="Update"
-                preview={preview}
-                setPreview={setPreview}
-            />
-        </Dialog>
+        <FormUser
+            form={form}
+            onSubmit={onSubmit}
+            isLoading={isPendingCreateUser}
+            type="Create"
+            preview={preview}
+            setPreview={setPreview}
+        />
     );
 }
