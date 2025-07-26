a317d9ca0e9fbee196a9b724bf254c6572ab0375
raddva <111165508+raddva@users.noreply.github.com>
2025-07-26 10:38:54 +0700

    add: table management

--- src/app/(dashboard)/admin/table/_components/table.tsx
+++ src/app/(dashboard)/admin/table/_components/table.tsx
@@ -11,15 +11,14 @@ import { useQuery } from '@tanstack/react-query';
 import { Pencil, Trash2 } from 'lucide-react';
 import { useMemo, useState } from 'react';
 import { toast } from 'sonner';
-import { Menu } from '@/validations/menu-validation';
-import Image from 'next/image';
-import { cn, convertIDR } from '@/lib/utils';
-import { HEADER_TABLE_MENU } from '@/constants/menu-constant';
-import DialogCreateMenu from './dialog-create-menu';
-import DialogUpdateMenu from './dialog-update-menu';
-import DialogDeleteMenu from './dialog-delete-menu';
+import { cn } from '@/lib/utils';
+import { Table } from '@/validations/table-validation';
+import { HEADER_TABLE_TABLE } from '@/constants/table-constant';
+import DialogCreateTable from './dialog-create-table';
+import DialogUpdateTable from './dialog-update-table';
+import DialogDeleteTable from './dialog-delete-table';
 
-export default function MenuManagement() {
+export default function TableManagement() {
     const supabase = createClient();
     const {
         currentPage,
@@ -30,28 +29,28 @@ export default function MenuManagement() {
         handleChangeSearch,
     } = useDataTable();
     const {
-        data: menus,
+        data: tables,
         isLoading,
         refetch,
     } = useQuery({
-        queryKey: ['menus', currentPage, currentLimit, currentSearch],
+        queryKey: ['tables', currentPage, currentLimit, currentSearch],
         queryFn: async () => {
             const query = supabase
-                .from('menus')
+                .from('tables')
                 .select('*', { count: 'exact' })
                 .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
                 .order('created_at');
 
             if (currentSearch) {
                 query.or(
-                    `name.ilike.%${currentSearch}%,category.ilike.%${currentSearch}%`,
+                    `name.ilike.%${currentSearch}%,description.ilike.%${currentSearch}%,status.ilike.%${currentSearch}%`,
                 );
             }
 
             const result = await query;
 
             if (result.error)
-                toast.error('Get Menu data failed', {
+                toast.error('Get Table data failed', {
                     description: result.error.message,
                 });
 
@@ -60,7 +59,7 @@ export default function MenuManagement() {
     });
 
     const [selectedAction, setSelectedAction] = useState<{
-        data: Menu;
+        data: Table;
         type: 'update' | 'delete';
     } | null>(null);
 
@@ -69,35 +68,22 @@ export default function MenuManagement() {
     };
 
     const filteredData = useMemo(() => {
-        return (menus?.data || []).map((menu: Menu, index) => {
+        return (tables?.data || []).map((table: Table, index) => {
             return [
                 currentLimit * (currentPage - 1) + index + 1,
-                <div className="flex items-center gap-2">
-                    <Image
-                        src={menu.image_url as string}
-                        alt={menu.name}
-                        width={40}
-                        height={40}
-                        className="rounded"
-                    />
-                    {menu.name}
-                </div>,
-                menu.category,
                 <div>
-                    <p>Base: {convertIDR(menu.price)}</p>
-                    <p>Discount: {menu.discount}</p>
-                    <p>
-                        After Discount:{' '}
-                        {convertIDR(menu.price - (menu.price * menu.discount) / 100)}
-                    </p>
+                    <h4 className="font-bold">{table.name}</h4>
+                    <p className="text-xs">{table.description}</p>
                 </div>,
+                table.capacity,
                 <div
-                    className={cn(
-                        'px-2 py-1 rounded-full text-white w-fit',
-                        menu.is_available ? 'bg-green-600' : 'bg-red-500',
-                    )}
+                    className={cn('px-2 py-1 rounded-full text-white w-fit capitalize', {
+                        'bg-green-600': table.status === 'available',
+                        'bg-red-600': table.status === 'unavailable',
+                        'bg-yellow-600': table.status === 'reserved',
+                    })}
                 >
-                    {menu.is_available ? 'Available' : 'Not Available'}
+                    {table.status}
                 </div>,
                 <DropdownAction
                     menu={[
@@ -110,7 +96,7 @@ export default function MenuManagement() {
                             ),
                             action: () => {
                                 setSelectedAction({
-                                    data: menu,
+                                    data: table,
                                     type: 'update',
                                 });
                             },
@@ -125,7 +111,7 @@ export default function MenuManagement() {
                             variant: 'destructive',
                             action: () => {
                                 setSelectedAction({
-                                    data: menu,
+                                    data: table,
                                     type: 'delete',
                                 });
                             },
@@ -134,18 +120,18 @@ export default function MenuManagement() {
                 />,
             ];
         });
-    }, [menus]);
+    }, [tables]);
 
     const totalPages = useMemo(() => {
-        return menus && menus.count !== null
-            ? Math.ceil(menus.count / currentLimit)
+        return tables && tables.count !== null
+            ? Math.ceil(tables.count / currentLimit)
             : 0;
-    }, [menus]);
+    }, [tables]);
 
     return (
         <div className="w-full">
             <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
-                <h1 className="text-2xl font-bold">Menu Management</h1>
+                <h1 className="text-2xl font-bold">Table Management</h1>
                 <div className="flex gap-2">
                     <Input
                         placeholder="Search..."
@@ -155,12 +141,12 @@ export default function MenuManagement() {
                         <DialogTrigger asChild>
                             <Button variant="outline">Create</Button>
                         </DialogTrigger>
-                        <DialogCreateMenu refetch={refetch} />
+                        <DialogCreateTable refetch={refetch} />
                     </Dialog>
                 </div>
             </div>
             <DataTable
-                header={HEADER_TABLE_MENU}
+                header={HEADER_TABLE_TABLE}
                 data={filteredData}
                 isLoading={isLoading}
                 totalPages={totalPages}
@@ -169,13 +155,13 @@ export default function MenuManagement() {
                 onChangePage={handleChangePage}
                 onChangeLimit={handleChangeLimit}
             />
-            <DialogUpdateMenu
+            <DialogUpdateTable
                 open={selectedAction !== null && selectedAction.type === 'update'}
                 refetch={refetch}
                 currentData={selectedAction?.data}
                 handleChangeAction={handleChangeAction}
             />
-            <DialogDeleteMenu
+            <DialogDeleteTable
                 open={selectedAction !== null && selectedAction.type === 'delete'}
                 refetch={refetch}
                 currentData={selectedAction?.data}
---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,9 @@
+import TableManagement from './_components/table';
+
+export const metadata = {
+    title: 'Mndy Cafe | Table Management',
+};
+
+export default function TableManagementPage() {
+    return <TableManagement />;
+}
